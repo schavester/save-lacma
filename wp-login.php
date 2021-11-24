d791303f7693e8d15d3c5d2c80a28cddbe37fbad
Richard Schave <schavester@gmail.com>
2021-11-23 18:26:07 -0800

    added press page
    added press page

--- wp-login.php
+++ wp-login.php
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="/wp-includes/js/wp-util.min.js?ver=5.7.3" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"9ba80dc881"};
+var userProfileL10n = {"user_id":"0","nonce":"6d7a87f692"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
